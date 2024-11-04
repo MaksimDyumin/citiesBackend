3a458e5142142d5277a4b7c40d228540c15d16fc
MaksimDyumin <maxdyumin@yandex.ru>
2024-11-04 20:13:17 +0300

    delete parse data logic before send

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express()
 app.use(cors())
 
 app.get('/peoples', async function (req, res) {
-    const result = {};
-
     try {
         const sqlResult = await pg`
             SELECT 
@@ -19,7 +17,8 @@ app.get('/peoples', async function (req, res) {
                 d.id AS districtId, 
                 d."name" AS districtName, 
                 c.id AS cityId, 
-                c."name" AS cityName, 
+                c."name" AS cityName,
+                c."data" AS cityData,
                 c2.id AS countryId, 
                 c2."name" AS countryName
             FROM 
@@ -29,40 +28,7 @@ app.get('/peoples', async function (req, res) {
             LEFT JOIN citys c ON c.id = d.citys_id 
             LEFT JOIN countrys c2 ON c2.id = c.country_id
         `;
-
-        for (let value of sqlResult) {
-            const countryName = value.countryname || 'Неуказанно';
-            if (!result[countryName]) {
-                result[countryName] = {};
-            }
-
-            const cityName = value.cityname || 'Неуказанно';
-            if (!result[countryName][cityName]) {
-                result[countryName][cityName] = {};
-            }
-
-            const districtName = value.districtname || 'Неуказанно';
-            if (!result[countryName][cityName][districtName]) {
-                result[countryName][cityName][districtName] = {};
-            }
-
-            const streetName = value.streetname || 'Неуказанно';
-            if (!result[countryName][cityName][districtName][streetName]) {
-                result[countryName][cityName][districtName][streetName] = {};
-            }
-
-            const homeName = value.home || 'Неуказанно';
-            if (!result[countryName][cityName][districtName][streetName][homeName]) {
-                result[countryName][cityName][districtName][streetName][homeName] = [];
-            }
-
-            result[countryName][cityName][districtName][streetName][homeName].push(value);
-            result[countryName]['countryid'] = value.countryid
-            result[countryName][cityName]['cityId'] = value.cityid
-            result[countryName][cityName][districtName]['districtId'] = value.districtid
-            result[countryName][cityName][districtName][streetName]['streetid'] = value.streetid
-        }
-        res.send(result);
+        res.send(sqlResult);
     } catch (error) {
         console.error('Ошибка при выполнении запроса:', error);
         res.status(500).send({ error: 'Произошла ошибка при получении данных' });
