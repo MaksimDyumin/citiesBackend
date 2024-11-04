0b989354d905b07ff1fb331d303deefd35b67ac7
MaksimDyumin <maxdyumin@yandex.ru>
2024-11-04 20:13:46 +0300

    update migrate.js

--- migrate.js
+++ migrate.js
@@ -1,25 +1,29 @@
 import { load } from 'migrate';
 import pg from './sql.js';
 
-load(
-  {
-    stateStore: './migrations/.migrate',
-    migrationsDirectory: './migrations',
-  },
-  function (err, set) {
-    if (err) {
-      throw err;
-    }
-    set.up(async function (err) {
+try {
+  load(
+    {
+      stateStore: './migrations/.migrate',
+      migrationsDirectory: './migrations',
+    },
+    function (err, set) {
       if (err) {
         throw err;
       }
-      console.log('Migrations successfully ran');
-      try {
-        await pg.end()
-      } catch (closeErr) {
-        console.error('Ошибка при завершении соединения:', closeErr);
-      }
-    });
-  }
-);
+      set.up(async function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log('Migrations successfully ran');
+        try {
+          await pg.end()
+        } catch (closeErr) {
+          console.error('Ошибка при завершении соединения:', closeErr);
+        }
+      });
+    }
+  );
+} catch (error) {
+}
+
