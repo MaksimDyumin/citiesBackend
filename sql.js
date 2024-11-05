d5092e7c4b40d17161842edfb99c9265ae249580
MaksimDyumin <maxdyumin@yandex.ru>
2024-11-05 03:53:06 +0300

    change db connect config

--- sql.js
+++ sql.js
@@ -1,11 +1,12 @@
-import postgres from 'postgres'
+import postgres from 'postgres';
 
+// Замените на ваши переменные окружения, если используете их
 const sql = postgres({ 
-    host: "localhost",
-    port: "5432",
-    username: "postgresUser",
-    password: "pgSuperSecretMnogaBycaBab",
-    database: "population",
- }) 
+    host: process.env.DB_HOST || "postgresDB",  // Используйте имя сервиса postgresDB
+    port: process.env.DB_PORT || 5432,
+    username: process.env.DB_USER || "postgresUser",
+    password: process.env.DB_PASSWORD || "pgSuperSecretMnogaBycaBab",
+    database: process.env.DB_NAME || "population",  // Имя базы данных должно быть "population"
+});
 
-export default sql
+export default sql;
