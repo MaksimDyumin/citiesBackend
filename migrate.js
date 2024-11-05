ed1b6f0d298d16008225e54381f09b596d6f26f3
MaksimDyumin <maxdyumin@yandex.ru>
2024-11-05 03:52:41 +0300

    change migrate file(add check migrate)

--- migrate.js
+++ migrate.js
@@ -1,29 +1,42 @@
 import { load } from 'migrate';
 import pg from './sql.js';
+import fs from 'node:fs';
 
-try {
-  load(
-    {
-      stateStore: './migrations/.migrate',
-      migrationsDirectory: './migrations',
-    },
-    function (err, set) {
-      if (err) {
-        throw err;
-      }
-      set.up(async function (err) {
+
+function migrate() {
+  try {
+    load(
+      {
+        stateStore: './migrations/.migrate',
+        migrationsDirectory: './migrations',
+      },
+      function (err, set) {
         if (err) {
           throw err;
         }
-        console.log('Migrations successfully ran');
-        try {
-          await pg.end()
-        } catch (closeErr) {
-          console.error('Ошибка при завершении соединения:', closeErr);
-        }
-      });
-    }
-  );
-} catch (error) {
+        set.up(async function (err) {
+          if (err) {
+            throw err;
+          }
+          console.log('Migrations successfully ran');
+          try {
+            await pg.end()
+          } catch (closeErr) {
+            console.error('Ошибка при завершении соединения:', closeErr);
+          }
+        });
+      }
+    );
+  } catch (error) {
+  }
 }
+
+const isAlreadyMigrated = fs.existsSync('./migrations/.migrate');
+if (!isAlreadyMigrated) {
+  migrate()
+} else {
+  console.log('Миграции уже были выполнены')
+}
+
+
 
